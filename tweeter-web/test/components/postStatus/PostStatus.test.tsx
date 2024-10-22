9832338f3787e15d94813f2197ab1064f83f3910
Helaman Sung <helamanjungminsung@gmail.com>
2024-10-22 16:11:29 -0600

    Created empty poststatus test

---
+++ tweeter-web/test/components/postStatus/PostStatus.test.tsx
@@ -0,0 +1,8 @@
+import PostStatus from "../../../src/components/postStatus/PostStatus";
+
+describe("PostStatus Component", () => {
+  it("Start with post and clear buttons disabled", () => {});
+  it("Enable both buttons when text field has text", () => {});
+  it("Disables both buttons if text field is cleared", () => {});
+  it("Calls presenter post method with correct parameters when post button is pressed", () => {});
+});
