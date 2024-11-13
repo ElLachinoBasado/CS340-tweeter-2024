530dc9a8b488f2fdaf0e11ad96bb4fec488b5c25
Helaman Sung <helamanjungminsung@gmail.com>
2024-11-12 23:16:58 -0700

    Fixed GetUserResponse and LoginResponse

--- tweeter-shared/src/model/net/response/GetUserResponse.ts
+++ tweeter-shared/src/model/net/response/GetUserResponse.ts
@@ -2,5 +2,5 @@ import { UserDTO } from "../../dto/UserDTO";
 import { TweeterResponse } from "./TweeterResponse";
 
 export interface GetUserResponse extends TweeterResponse {
-  readonly user: UserDTO;
+  readonly user: UserDTO | null;
 }
--- tweeter-shared/src/model/net/response/LoginResponse.ts
+++ tweeter-shared/src/model/net/response/LoginResponse.ts
@@ -1,7 +1,8 @@
 import { AuthTokenDTO } from "../../dto/AuthTokenDTO";
 import { UserDTO } from "../../dto/UserDTO";
+import { TweeterResponse } from "./TweeterResponse";
 
-export interface LoginResponse {
+export interface LoginResponse extends TweeterResponse {
   readonly user: UserDTO;
   readonly token: AuthTokenDTO;
 }
